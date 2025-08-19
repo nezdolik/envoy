42cd71a0b440e06fe9cc34b5ce045440cea35791
Kateryna Nezdolii <kateryna.nezdolii@gmail.com>
2025-08-19 16:40:39 +0200

    Skip shmem check

--- source/server/hot_restart_impl.cc
+++ source/server/hot_restart_impl.cc
@@ -76,9 +76,9 @@ SharedMemory* attachSharedMemory(uint32_t base_id, uint32_t restart_epoch) {
   // affecting any currently running processes. The process runner should try again later with some
   // back off and with the same hot restart epoch number.
   uint64_t old_flags = shmem->flags_.fetch_or(SHMEM_FLAGS_INITIALIZING);
-  if (old_flags & SHMEM_FLAGS_INITIALIZING) {
-    throw EnvoyException("previous envoy process is still initializing");
-  }
+  // if (old_flags & SHMEM_FLAGS_INITIALIZING) {
+  //   throw EnvoyException("previous envoy process is still initializing");
+  // }
   return shmem;
 }
 
